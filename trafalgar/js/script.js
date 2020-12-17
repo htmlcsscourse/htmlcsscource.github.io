f8d27d319f540ff215f8868071479d8ea25921df
= <=>
2020-12-18 01:35:14 +0200

    slider

---
+++ trafalgar/js/script.js
@@ -0,0 +1,8 @@
+$('.feedback__slider').slick({
+    dots: true,
+    arrow: true,
+    infinite: false,
+    prevArrow: $('#prev'),
+    nextArrow: $('#next'),
+    appendDots: $('.feedback__dots'),
+});
