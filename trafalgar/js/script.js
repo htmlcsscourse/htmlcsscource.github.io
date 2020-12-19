fd60f0d134f53ad07bea6014505fdf0bbba96a3e
test <73775955+htmlcsscourse@users.noreply.github.com>
2020-12-20 01:07:58 +0200

    added animation + logic for View all button

--- trafalgar/js/script.js
+++ trafalgar/js/script.js
@@ -23,5 +23,27 @@ function scrollFunction() {
         btnGoTop.classList.add('btn__go-top--show');
     } else {
         btnGoTop.classList.remove('btn__go-top--show');
+        btnGoTop.fadeOut(300);
     }
 }
+
+const btnViewAll = document.querySelector('.info__btn');
+btnViewAll.addEventListener('click', function () {
+    if (btnViewAll.classList.contains('info__btn--all')) {
+        btnViewAll.classList.remove('info__btn--all');
+        btnViewAll.textContent = "View all";
+
+        var items = document.querySelectorAll('.info__item');
+        for (let i = 3; i < items.length; i++) {
+            items[i].classList.add('info__item--hide');
+        }
+    } else {
+        btnViewAll.classList.add('info__btn--all');
+        btnViewAll.textContent = "View less";
+
+        var items = document.querySelectorAll('.info__item');
+        for (let i = 3; i < items.length; i++) {
+            items[i].classList.remove('info__item--hide');
+        }
+    }
+});
