33c6fe15bf1e76b987f3b22990e9e9780b4307ca
test <73775955+htmlcsscourse@users.noreply.github.com>
2020-12-19 22:43:35 +0200

    go top button

--- trafalgar/js/script.js
+++ trafalgar/js/script.js
@@ -6,3 +6,23 @@ $('.feedback__slider').slick({
     nextArrow: $('#next'),
     appendDots: $('.feedback__dots'),
 });
+
+//Get the button:
+const btnGoTop = document.getElementsByClassName("btn__go-top")[0];
+btnGoTop.addEventListener('click', () => window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+}));
+
+
+// When the user scrolls down 400px from the top of the document, show the button
+window.onscroll = function () { scrollFunction(); };
+
+function scrollFunction() {
+    console.log(1);
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        btnGoTop.classList.add('btn__go-top--show');
+    } else {
+        btnGoTop.classList.remove('btn__go-top--show');
+    }
+}
